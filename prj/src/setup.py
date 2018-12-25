08e0a8152cac539dc29e98887ff78f7dbac47675
J4YP33 <jonathanpung@gmail.com>
2018-12-26 06:22:19 +0800

    added launch json for building

--- prj/src/setup.py
+++ prj/src/setup.py
@@ -3,4 +3,4 @@ from cx_Freeze import setup, Executable
 setup(name = "POSTAGE" ,
       version = "0.1" ,
       description = "" ,
-      executables = [Executable("postage.py")])
+      executables = [Executable("mailMerge.py")])
